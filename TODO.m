1c8b840e43ed2e9d84554534589ea37d861f5f1e
CrazySwimmer <danilo.zocco@gmail.com>
2018-01-06 05:07:50 +0100

    Updated TODO

--- TODO.m
+++ TODO.m
@@ -1,4 +1,8 @@
 %% List of pricing functions
+%
+% Checks for functions: comments, doc(description, inputs, outputs, sources, name, date),
+% indents
+%
 % ===== European vanilla options =====
 % BS for European vanilla put -> BS_EU_std : Doc and comment
 % BT for European vanilla put -> BT_EU_std : Doc and comment
@@ -6,12 +10,12 @@
 
 % ===== American vanilla options =====
 % BT for American vanilla put -> BT_AM_std : Doc and comment
-% MC for American vanilla put -> MC_AM_std :
+% MC for American vanilla put -> MC_AM_std : Refine, doc and comment + Add Laguerre poly
 
 % ===== European binary options (cash) =====
 % BS for European binary put -> BS_EU_bin : Doc and comment
 % BT for European binary put -> BT_EU_bin : Doc and comment
-% MC for European binary put -> MC_EU_bin : Refine, doc and comment + Add Laguerre poly
+% MC for European binary put -> MC_EU_bin : Doc and comment
 
 % ===== American binary options (cash) =====
 % BT for American binary put -> BT_AM_bin : Doc and comment
@@ -19,18 +23,22 @@
 
 
 %% ===== Notes =====
-% S0 : Initial stock price
-% K  : Strike
-% r  : continuously-compunded risk-free rate
+% S     : Initial stock price
+% K     : Strike
+% B     : Barrier
+% r     : Continuously-compunded risk-free rate
+% t     : Time-to-expiry
+% sigma : Annual volatility
+% n     : Number of steps for binomial methods
+% N     : Number of steps for Monte Carlo methods
+% M     : Number of paths for Monte Carlo methods
+
 
 %% ===== Ideas and thoughts =====
 % Modify binaries for A (qty)
 % Add Greeks !
-% Add Asian options
 % Make a live script version and publish to HTML
 % Check coherence across variables S,K,r,T,t,sigma,etc.
-%  --> Coherence must be across models, not across functions
 % Check use of seed
-% Decide about tolerance and precision across diferent methods
+% Decide about tolerance and precision across different methods (?)
 % Add section about improvements (var reduc, etc.)
-%
